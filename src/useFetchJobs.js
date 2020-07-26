f11c128c482954715c709ee68d78888dfc871d60
Annie Huang <annie.hn@gmail.com>
2020-07-26 13:35:50 +1000

    Return different state according the the action.type

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -13,11 +13,11 @@ function reducer(state, action) {
 
     switch (action.type) {
         case ACTIONS.MAKE_REQUEST:
-
+            return {loading: true, jobs: []}
         case ACTIONS.GET_DATA:
-
+            return {...state, loading: false, jobs: action.payload.jobs}
         case ACTIONS.ERROR:
-
+            return {...state, loading: false, jobs: [], error: action.payload.error}
         default:
             return state
     }
