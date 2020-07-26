dc08b4164c0d6487c98d1cee4de60a7d1b8dcb7e
Annie Huang <annie.hn@gmail.com>
2020-07-26 13:31:16 +1000

    Setup reducer structure

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,7 +1,35 @@
+import {useReducer} from "react";
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+
+
+function reducer(state, action) {
+    // e.g. if you have dispatch({type: 'hello', payload: {x:3}})
+    // then action.payload.x is 3.
+
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST:
+
+        case ACTIONS.GET_DATA:
+
+        case ACTIONS.ERROR:
+
+        default:
+            return state
+    }
+}
+
 export default function useFetchJobs(params, page) {
+    // useReducer{fn, <initialState>}
+    const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true});
+
     return {
-        jobs: [4,5,6],
-        loading: true,
-        error: true
+        jobs: [],
+        loading: false,
+        error: false
     }
 }
