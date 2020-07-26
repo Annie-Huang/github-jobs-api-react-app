afc58f011d503d78bc45251a7d945bd1e0a6218f
Annie Huang <annie.hn@gmail.com>
2020-07-26 22:15:59 +1000

    Retrieve hasNextPage in App.js and pass it to the JobsPagination component

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import JobsPagination from "./JobsPagination";
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1)
-  const {jobs, loading, error} = useFetchJobs(params, page);
+  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page);
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map(job => <Job key={job.id} job={job}/>)}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   )
 }
