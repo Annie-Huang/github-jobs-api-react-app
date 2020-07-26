3aabfe5a2f3cf9bd7380a14b3fe3959692dc2e93
Annie Huang <annie.hn@gmail.com>
2020-07-26 18:28:07 +1000

    Add company name in.

--- src/Job.js
+++ src/Job.js
@@ -3,15 +3,17 @@ import {Card} from "react-bootstrap";
 
 const Job = ({job}) => {
   return (
-    <Card.Body>
-      <div className="d-flex justify-content-between">
-        <div>
-          <Card.Title>
-            {job.title}
-          </Card.Title>
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            </Card.Title>
+          </div>
         </div>
-      </div>
-    </Card.Body>
+      </Card.Body>
+    </Card>
   );
 };
 
