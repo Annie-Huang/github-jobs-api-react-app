649bd3a3a7b7f15456fae5044849c8a898b3b986
Annie Huang <annie.hn@gmail.com>
2020-07-27 21:28:10 +1000

    Add styling to make all three fields align vertically.

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -12,7 +12,7 @@ positions.json?markdown=true&page=1&[object+Object]=United+States
 const SearchForm = ({params, onParamChange}) => {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
@@ -22,7 +22,7 @@ const SearchForm = ({params, onParamChange}) => {
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
         </Form.Group>
         <Form.Group as={Col} xs="auto" className="ml-2">
-          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" label="Only Full Time" type="checkbox"/>
+          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" label="Only Full Time" type="checkbox" className="mb-2" />
         </Form.Group>
       </Form.Row>
     </Form>
