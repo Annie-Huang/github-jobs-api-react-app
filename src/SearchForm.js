56ff355d54cb4e5b5bb4418f4a7597133739dc08
Annie Huang <annie.hn@gmail.com>
2020-07-27 21:26:13 +1000

    Add Checkbox for Full time only into the Form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -6,6 +6,8 @@ import {Form, Col} from 'react-bootstrap';
 When you search for Management in Description: the URL will becomes:
 http://localhost:3000/positions.json?markdown=true&page=2&[object+Object]=Management
 
+When you search for Management in Location: the URL will becomes:
+positions.json?markdown=true&page=1&[object+Object]=United+States
  */
 const SearchForm = ({params, onParamChange}) => {
   return (
@@ -18,6 +20,9 @@ const SearchForm = ({params, onParamChange}) => {
         <Form.Group as={Col}>
           <Form.Label>Location</Form.Label>
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" label="Only Full Time" type="checkbox"/>
         </Form.Group>
       </Form.Row>
     </Form>
